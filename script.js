97aa864e331f93998cb42d2ba593971e82e543ca
Sadid Ethun <ethun2004@gmail.com>
2024-02-28 00:48:05 -0500

    Added background and changed format

--- script.js
+++ script.js
@@ -5,16 +5,10 @@ console.log(projects);
 
 const myName = document.getElementById("myName");
 myName.textContent = 'Sadid Ethun';
-myName.style.color = 'darkblue';
+myName.style.color = 'black';
 
 
-let clicks = 0;
-myName.addEventListener("click", function () {
-    if (clicks % 2 == 0) {
-        myName.style.color = 'darkblue';
-    } else { myName.style.color = 'lightblue'; }
-    clicks += 1;
-});
+
 
 const project1btn = document.getElementById("project1btn");
 const project2btn = document.getElementById("project2btn");
