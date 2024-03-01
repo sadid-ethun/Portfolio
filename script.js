67f7aa0d5d05ef867fcc45e564bed8c362695b1a
Sadid Ethun <ethun2004@gmail.com>
2024-02-29 21:00:57 -0500

    Added scrolling transition

--- script.js
+++ script.js
@@ -31,3 +31,17 @@ githubImg.addEventListener("click", function () {
 linkedinImg.addEventListener("click", function () {
     window.open("https://linkedin.com/in/sadid-ethun")
 });
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
